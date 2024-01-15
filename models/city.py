f288dd43615302acb7b92bb734582d69c368c156
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-15 22:23:10 +0000

    all

--- models/city.py
+++ models/city.py
@@ -7,6 +7,7 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
+
 class City(BaseModel):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from os import getenv
+from models.base_model import Base
+import models
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from Sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref"state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all City objects related"""
+            list_city = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
