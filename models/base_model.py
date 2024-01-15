750aa53ce57eda90dd8843775bc1e46479e7a5f3
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-15 22:10:31 +0000

    all

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,16 @@
 import uuid
 from datetime import datetime
 
+Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True, bullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
