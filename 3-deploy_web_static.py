a893d31c97a93751f9e81e23617231fb485181ae
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-02-01 20:53:38 +0000

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["34.229.56.17", "54.146.78.115"]
 
 
 def do_pack():
