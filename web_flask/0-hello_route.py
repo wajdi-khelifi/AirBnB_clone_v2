dc7b4b4cd848d1630757a77a3e80b268d20ab9c8
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-02-16 21:43:17 +0100

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Define the route for the root URL 
+@app.route("/", strict_slashes=False)
+def hello():
+    """Display 'Hello HBNB'"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
